5d0956eb7cec1e828fd774e67f3958fea4128da2
Naina paliwal <157493330+Niyaa04@users.noreply.github.com>
2026-06-04 12:59:37 +0530

    Update portfolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my_portfolio');
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+
+    const x =
+      (window.innerWidth / 2 - event.clientX) / 40;
+
+    const y =
+      (window.innerHeight / 2 - event.clientY) / 40;
+
+    const circle =
+      document.getElementById('circle');
+
+    const square =
+      document.getElementById('square');
+
+    const triangle =
+      document.getElementById('triangle');
+
+    if (circle) {
+      circle.style.transform =
+        `translate(${x}px, ${y}px)`;
+    }
+
+    if (square) {
+      square.style.transform =
+        `translate(${x * 1.5}px, ${y * 1.5}px)`;
+    }
+
+    if (triangle) {
+      triangle.style.transform =
+        `translate(${x * 0.8}px, ${y * 0.8}px)`;
+    }
+
+  }
 }
